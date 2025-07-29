a474a2a8abd4fd2fecd54165aa9536b3d499cffa
Mallart10 <mallart.apps@gmail.com>
2025-07-29 09:03:58 +0100

    rutas creadas

--- gestor-actividades-escolares/routes/web.php
+++ gestor-actividades-escolares/routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ActividadController;
+use App\Http\Controllers\AlumnoController;
+use App\Http\Controllers\InscripcionController;
 
-Route::get('/actividades', [ActividadController::class, 'index'])->name('actividades.index');
+Route::resource('actividades', ActividadController::class);
+Route::resource('alumnos', AlumnoController::class);
+Route::post('/inscripciones', [InscripcionController::class, 'store'])->name('inscripciones.store');
